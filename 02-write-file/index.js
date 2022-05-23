eaf4a83f08cc090fb9d5eb18f7eee4e5b44b77ae
dmitry <Villigo701@gmail.com>
2022-05-23 09:31:47 +0300

    feat:add 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+let file = fs.createWriteStream(path.join(__dirname, './text.txt'), 'utf-8');
+process.stdout.write('Прывітанне сябар. Калі ласка, увядзі слова на беларускай мове:\n');
+
+process.stdin.on('data', data => {
+    if (data.toString().trim() === 'exit') {
+        process.stdout.write('Да пабачэння');
+        process.exit();
+    }
+    file.write(data);
+})
