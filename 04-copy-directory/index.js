14acb9105e69cc13bd61f32253ffa6863040b6d8
dmitry <Villigo701@gmail.com>
2022-05-23 17:22:50 +0300

    feat: add 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.rm(path.join(__dirname, 'files-copy'), {force: true}, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) =>{
+        if(err) return console.error(err);
+        console.log('Directory created successfully!');
+    });
+    fs.readdir(path.join(__dirname, 'files'), (err, data) => {
+        if (err) {
+            console.log(err)
+        }  
+        data.forEach((element) => {    
+            fs.copyFile(path.join(__dirname, 'files', element), path.join(__dirname, 'files-copy', element), (err)=>{
+                if(err) return console.error(err);
+            })    
+        })
+    })  
+});
