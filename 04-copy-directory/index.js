827ad156ca0bddcf4c43cb9667fcd3070518f38c
dmitry <Villigo701@gmail.com>
2022-05-24 13:43:20 +0300

    fix: delete file 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,9 +3,25 @@ const path = require('path');
 
 fs.rm(path.join(__dirname, 'files-copy'), {force: true}, () => {
     fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) =>{
-        if(err) return console.error(err);
+        if (err) {
+            console.log(err)
+        } 
         console.log('Directory created successfully!');
     });
+
+    fs.readdir(path.join(__dirname, 'files-copy'), (err, data) =>{
+        if (err) {
+            console.log(err)
+        } 
+        if(data){
+            data.forEach((element) =>{
+                fs.unlink(path.join(__dirname, 'files-copy', element), (err) =>{
+                    if(err) return console.error(err);
+                })
+            })
+        }
+    })
+
     fs.readdir(path.join(__dirname, 'files'), (err, data) => {
         if (err) {
             console.log(err)
@@ -17,3 +33,6 @@ fs.rm(path.join(__dirname, 'files-copy'), {force: true}, () => {
         })
     })  
 });
+
+
+
